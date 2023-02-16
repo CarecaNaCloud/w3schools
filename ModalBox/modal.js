3791e4c678b93d59ec8a5f9746b73dfadb9a7142
CarecaNaCloud <107486346+CarecaNaCloud@users.noreply.github.com>
2023-02-16 18:05:13 -0300

    Documentating modal

--- ModalBox/modal.js
+++ ModalBox/modal.js
@@ -6,14 +6,18 @@ let modalBtn = document.getElementById("modalBtn");
 
 let closeBtn = document.getElementsByClassName("modal-closeBtn")[0];
 
+
+//Display Modal
 modalBtn.onclick = function() {
     modal.style.display = "block";
 }
 
+//Close Modal on X button click
 closeBtn.onclick = function() {
     modal.style.display = "none";
 }
 
+//Close Modal on window click
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
